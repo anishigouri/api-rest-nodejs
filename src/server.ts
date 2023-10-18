81759217e1cbb6cf31b07da5246af6f08625bf41
Allan Nishigouri <allan.nishigouri@codex.com.br>
2023-10-18 16:41:54 -0300

    Adcionado novo campo a tabela transaction

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  return await knex('transactions').select('*')
 })
 
 app
